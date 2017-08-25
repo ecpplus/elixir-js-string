0d9b7e669d5f17df487317af486c335c1d5d5df2
chu <ecpplus@gmail.com>
2017-08-25 20:35:01 +0900

    bump version to 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JSString.Mixfile do
   def project do
     [
       app: :js_string,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       description: description(),
